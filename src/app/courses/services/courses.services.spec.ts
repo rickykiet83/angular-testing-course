cc7707107cc82bfc027eb3ac47b6c6926122158d
Kiet Pham <kietpham.dev@gmail.com>
2021-08-14 18:54:46 +1000

    Angular HTTP Service Testing - Step-by-Step Implementation

--- src/app/courses/services/courses.services.spec.ts
+++ src/app/courses/services/courses.services.spec.ts
@@ -1,29 +1,50 @@
+import { COURSES, findLessonsForCourse } from '../../../../server/db-data';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
+import { Course } from '../model/course';
 import { CoursesService } from './courses.service';
+import { HttpErrorResponse } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 describe('CoursesService', () => {
+
   let coursesService: CoursesService,
     httpTestingController: HttpTestingController;
 
   beforeEach(() => {
 
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [
+        HttpClientTestingModule
+      ],
       providers: [
         CoursesService
       ]
     });
-    coursesService = TestBed.inject(CoursesService);
-    httpTestingController = TestBed.inject(HttpTestingController);
+
+    coursesService = TestBed.inject(CoursesService),
+      httpTestingController = TestBed.inject(HttpTestingController);
 
   });
 
   it('should retrieve all courses', () => {
 
+    coursesService.findAllCourses()
+      .subscribe(courses => {
 
+        expect(courses).toBeTruthy('No courses returned');
 
+        expect(courses.length).toBe(12,
+          "incorrect number of courses");
+
+        const course = courses.find(course => course.id === 12);
+        expect(course.titles.description).toBe('Angular Testing Course');
+      });
+
+    const req = httpTestingController.expectOne('/api/courses');
+    expect(req.request.method).toEqual('GET');
+    req.flush({ payload: Object.values(COURSES) });
   });
 
 });
+
