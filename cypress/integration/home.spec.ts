5a6eab7152ecb37fea4f0287def78f121f9e2ac7
Kiet Pham <kietpham.dev@gmail.com>
2021-08-15 22:30:46 +1000

    How to Mock an HTTP Backend using Cypress

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,6 +1,23 @@
 describe("Home Page", () => {
+
   it("should display a list of courses", () => {
+
+    cy.fixture('courses.json').as('coursesJSON');
+
+    // must have
+    cy.server();
+
+    // before route
+    cy.route('/api/courses', '@coursesJSON').as('courses');
+
     cy.visit("/");
+
     cy.contains("All Courses");
+
+    cy.wait('@courses');
+
+    cy.get('mat-card').should('have.length', 9);
+
+
   });
 });
