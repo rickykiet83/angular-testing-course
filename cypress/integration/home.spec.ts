ba37e4c7eaa35a1d37c4fa47f022dc37c75dcba7
Kiet Pham <kietpham.dev@gmail.com>
2021-08-15 22:36:32 +1000

    Simulating User Interaction in Cypress End to End Tests

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,7 +1,6 @@
 describe("Home Page", () => {
 
-  it("should display a list of courses", () => {
-
+  beforeEach(() => {
     cy.fixture('courses.json').as('coursesJSON');
 
     // must have
@@ -11,6 +10,9 @@ describe("Home Page", () => {
     cy.route('/api/courses', '@coursesJSON').as('courses');
 
     cy.visit("/");
+  });
+
+  it("should display a list of courses", () => {
 
     cy.contains("All Courses");
 
@@ -18,6 +20,19 @@ describe("Home Page", () => {
 
     cy.get('mat-card').should('have.length', 9);
 
+  });
+
+  it('should display the advanced courses', () => {
+
+    cy.get('.mat-tab-label').should('have.length', 2);
+
+    cy.get('.mat-tab-label').last().click();
+
+    cy.get('.mat-tab-body-active .mat-card-title').its('length').should('be.gt', 1);
+    cy.get('.mat-tab-body-active .mat-card-title').first()
+      .should('contain', 'Angular Security Course');
+
 
   });
+
 });
