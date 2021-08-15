366e5aa851ff0ea061588e1431bdcec2efefcbb1
Kiet Pham <kietpham.dev@gmail.com>
2021-08-15 14:40:08 +1000

    Testing Angular Smart or Container Components - Test Setup

---
+++ src/app/courses/home/home.component.spec.ts
@@ -0,0 +1,75 @@
+import { ComponentFixture, TestBed, async, fakeAsync, flush, flushMicrotasks, waitForAsync } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+import { By } from '@angular/platform-browser';
+import { COURSES } from '../../../../server/db-data';
+import { CoursesModule } from '../courses.module';
+import { CoursesService } from '../services/courses.service';
+import { DebugElement } from '@angular/core';
+import { HomeComponent } from './home.component';
+import { HttpClient } from '@angular/common/http';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { click } from '../common/test-utils';
+import { of } from 'rxjs';
+import { setupCourses } from '../common/setup-test-data';
+
+describe('HomeComponent', () => {
+
+  let fixture: ComponentFixture<HomeComponent>;
+  let component: HomeComponent;
+  let el: DebugElement;
+
+  beforeEach(waitForAsync(() => {
+    const coursesServiceSpy = jasmine.createSpyObj('CoursesService', ['findAllCourses']);
+    TestBed.configureTestingModule({
+      imports: [
+        CoursesModule,
+        NoopAnimationsModule,
+      ],
+      providers: [{ provide: CoursesService, useValue: coursesServiceSpy }]
+    }).compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(HomeComponent);
+        component = fixture.componentInstance;
+        el = fixture.debugElement;
+      })
+
+  }));
+
+  it("should create the component", () => {
+
+    expect(component).toBeTruthy();
+
+  });
+
+
+  it("should display only beginner courses", () => {
+
+    pending();
+
+  });
+
+
+  it("should display only advanced courses", () => {
+
+    pending();
+
+  });
+
+
+  it("should display both tabs", () => {
+
+    pending();
+
+  });
+
+
+  it("should display advanced courses when tab clicked", () => {
+
+    pending();
+
+  });
+
+});
+
+
