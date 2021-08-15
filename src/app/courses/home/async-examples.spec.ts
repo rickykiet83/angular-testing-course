fa76076e8c343028c25bcf1721e5234abe053357
Kiet Pham <kietpham.dev@gmail.com>
2021-08-15 17:27:39 +1000

    Using fakeAsync to test Asynchronous Observables

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,5 +1,8 @@
 import { fakeAsync, flush, flushMicrotasks, tick } from "@angular/core/testing";
 
+import { delay } from "rxjs/operators";
+import { of } from "rxjs/internal/observable/of";
+
 describe('Async Testing Examples', () => {
 
   it('Asynchronous test example with Jasmine done()', (done: DoneFn) => {
@@ -52,7 +55,7 @@ describe('Async Testing Examples', () => {
     expect(test).toBeTruthy();
   }));
 
-  fit('Asynchronous test example - Promise + setTimeout()', fakeAsync(() => {
+  it('Asynchronous test example - Promise + setTimeout()', fakeAsync(() => {
 
     let counter = 0;
 
@@ -78,6 +81,26 @@ describe('Async Testing Examples', () => {
     tick(500);
 
     expect(counter).toBe(11);
+
+  }));
+
+  it('Asynchronous test example - Observables', fakeAsync(() => {
+
+    let test = false;
+
+    console.log('Creating Observable');
+
+    const test$ = of(test).pipe(delay(1000));
+
+    test$.subscribe(() => {
+      test = true;
+    });
+
+    tick(1000);
+
+    console.log('Running test assertions');
+
+    expect(test).toBe(true);
 
   }));
 
