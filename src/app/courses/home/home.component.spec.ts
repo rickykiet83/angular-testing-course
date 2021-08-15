36fe2d6981d97723ace99123b8333c4706058377
Kiet Pham <kietpham.dev@gmail.com>
2021-08-15 18:06:51 +1000

    Understanding the Angular async() Test Zone

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -87,7 +87,7 @@ describe('HomeComponent', () => {
   });
 
 
-  fit("should display advanced courses when tab clicked", fakeAsync(() => {
+  it("should display advanced courses when tab clicked", fakeAsync(() => {
 
     coursesService.findAllCourses.and.returnValue(of(setupCourses()));
 
@@ -109,6 +109,29 @@ describe('HomeComponent', () => {
 
   }));
 
+  fit("should display advanced courses when tab clicked - async", waitForAsync(() => {
+
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css('.mat-tab-label'));
+
+    click(tabs[1]);
+
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      console.log('called whenStable()');
+
+      const cardTitles = el.queryAll(By.css('.mat-tab-body-active .mat-card-title'));
+
+      expect(cardTitles.length).toBeGreaterThan(0, 'Could not find card titles');
+
+      expect(cardTitles[0].nativeElement.textContent).toContain('Angular Security Course');
+    });
+
+
+  }));
+
 });
-
-
